2e3062cf0522368c0eebe297004c21b2059de03f
Jeffrey Rennie <rennie@google.com>
2015-09-30 12:37:59 -0700

    Start bigquery sample.

---
+++ bigquery/main.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @return Google_Service_Bigquery
+ * @throws Exception
+ */
+function createAuthorizedClient()
+{
+    $json_credentials_path = getenv('GOOGLE_APPLICATION_CREDENTIALS');
+    if (!$json_credentials_path) {
+        throw new Exception('Set the environment variable ' .
+            'GOOGLE_APPLICATION_CREDENTIALS to the path to your .json file.');
+    }
+    $json_a = json_decode(file_get_contents($json_credentials_path));
+    $credentials = new Google_Auth_AssertionCredentials(
+        $json_a['client_email'],
+        array(Google_Service_Bigquery::BIGQUERY),
+        $json_a['private_key']
+    );
+    $client = new Google_client();
+    $client->setAssertionCredentials($credentials);
+    if ($client->getAuth()->isAccessTokenExpired()) {
+        $client->getAuth()->refreshTokenWithAssertion();
+    }
+    $service = new Google_Service_Bigquery($client);
+    return $service;
+}
+
+$client = createAuthorizedClient();
