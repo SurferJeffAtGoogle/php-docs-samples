bf3c36efe5c6f90718194bd6d36d7960db81b796
Jeffrey Rennie <rennie@google.com>
2015-10-06 14:03:32 -0700

    Add copyright and doc import markers.

--- bigquery/main.php
+++ bigquery/main.php
@@ -1,7 +1,21 @@
 <?php
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 require_once __DIR__ . '/vendor/autoload.php';
-
+// [START all]
+// [START build_service]
 /**
  * Create an authorized client that we will use to invoke BigQuery.
  * @return Google_Service_Bigquery
@@ -29,7 +43,7 @@ function createAuthorizedClient()
     $service = new Google_Service_Bigquery($client);
     return $service;
 }
-
+// [END build_service]
 $bigquery = createAuthorizedClient();
 $projectId = '';
 if ($projectId) {
@@ -41,13 +55,16 @@ if ($projectId) {
     $projectId = trim(fgets(STDIN));
 }
 
+// [START run_query]
 // Pack a BigQuery request.
 $request = new Google_Service_Bigquery_QueryRequest();
 $request->setQuery('SELECT TOP(corpus, 10) as title, COUNT(*) as unique_words ' .
     'FROM [publicdata:samples.shakespeare]');
 $response = $bigquery->jobs->query($projectId, $request);
 $rows = $response->getRows();
+// [END run_query]
 
+// [START print_results]
 // Print the results to stdout in a human-readable way.
 echo "\nQuery Results:\n------------\n";
 foreach ($rows as $row) {
@@ -56,3 +73,5 @@ foreach ($rows as $row) {
     }
     echo "\n";
 }
+// [END print_results]
+// [END all]
