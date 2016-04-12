fbe8f28812df6b4a17c6d65829ed8f7b7d10f73e
Jeffrey Rennie <rennie@google.com>
2016-04-12 10:16:06 -0700

    It works.

--- appengine/standard/users/functions.php
+++ appengine/standard/users/functions.php
@@ -8,11 +8,11 @@ function getGreeting()
     $user = UserService::getCurrentUser();
 
     if (isset($user)) {
-        echo sprintf('Welcome, %s! (<a href="%s">sign out</a>)',
+        return sprintf('Welcome, %s! (<a href="%s">sign out</a>)',
             $user->getNickname(),
             UserService::createLogoutUrl('/'));
     } else {
-        echo sprintf('<a href="%s">Sign in or register</a>',
+        return sprintf('<a href="%s">Sign in or register</a>',
             UserService::createLoginUrl('/'));
     }
 }
