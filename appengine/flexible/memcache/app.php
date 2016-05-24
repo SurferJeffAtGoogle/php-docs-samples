67779e1fb1fd9aae894ef2aa5c775e9789db788d
Jeffrey Rennie <rennie@google.com>
2016-05-24 10:43:31 -0700

    Call addServer() for every Memcache and Memcached instance.  Add a /vars handler that dumps the memcache-related environment variables.

--- appengine/flexible/memcache/app.php
+++ appengine/flexible/memcache/app.php
@@ -18,16 +18,39 @@
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 // create the Silex application
 $app = new Application();
 $app->register(new TwigServiceProvider());
 $app['twig.path'] = [ __DIR__ ];
 
+function addServer(Memcached $memcached) {
+    $addr = getenv('MEMCACHE_PORT_11211_TCP_ADDR');
+    $port = getenv('MEMCACHE_PORT_11211_TCP_PORT');
+    if (!$memcached->addServer($addr, $port))
+        throw new Exception("Failed to add server $addr:$port");
+    return $memcached;
+}
+
+$app->get('/vars', function() {
+    $vars = array('MEMCACHE_PORT_11211_TCP_ADDR',
+        'MEMCACHE_PORT_11211_TCP_PORT');
+    $lines = array();
+    foreach ($vars as $var) {
+        $val = getenv($var);
+        array_push($lines, "$var = $val");
+    }
+    return new Response(
+        implode("\n", $lines),
+        200,
+        ['Content-Type' => 'text/plain']);
+});
+
 $app->get('/', function (Application $app, Request $request) {
     /** @var Twig_Environment $twig */
     $twig = $app['twig'];
-    $memcache = new Memcached;
+    $memcache = addServer(new Memcached);
     return $twig->render('memcache.html.twig', [
         'who' => $memcache->get('who'),
         'count' => $memcache->get('count'),
@@ -39,7 +62,7 @@ $app->post('/', function (Application $app, Request $request) {
     /** @var Twig_Environment $twig */
     $twig = $app['twig'];
     # [START who_count]
-    $memcache = new Memcached;
+    $memcache = addServer(new Memcached);
     $memcache->set('who', $request->get('who'));
     return $twig->render('memcache.html.twig', [
         'who' => $request->get('who'),
@@ -52,14 +75,14 @@ $app->post('/', function (Application $app, Request $request) {
 // Simple HTTP GET and PUT operators.
 $app->get('/memcache/{key}', function ($key) {
     # [START memcache_get]
-    $memcache = new Memcache;
+    $memcache = addServer(new Memcache);
     return $memcache->get($key);
     # [END memcache_get]
 });
 
 $app->put('/memcache/{key}', function ($key, Request $request) {
     # [START memcache_put]
-    $memcache = new Memcache;
+    $memcache = addServer(new Memcache);
     $value = $request->getContent();
     return $memcache->set($key, $value);
     # [END memcache_put]
@@ -67,14 +90,14 @@ $app->put('/memcache/{key}', function ($key, Request $request) {
 
 $app->get('/memcached/{key}', function ($key) {
     # [START memcached_get]
-    $memcache = new Memcached;
+    $memcache = addServer(new Memcached);
     return $memcache->get($key);
     # [END memcached_get]
 });
 
 $app->put('/memcached/{key}', function ($key, Request $request) {
     # [START memcached_put]
-    $memcache = new Memcached;
+    $memcache = addServer(new Memcached);
     $value = $request->getContent();
     return $memcache->set($key, $value);
     # [END memcached_put]
