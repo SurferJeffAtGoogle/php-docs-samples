458a4b6cbaf1b6429934bf28c3f1bf53e0eeb544
Jeffrey Rennie <rennie@google.com>
2016-09-16 13:55:58 -0700

    AnalyzeSentiment

--- language/api/language.php
+++ language/api/language.php
@@ -3,8 +3,10 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Google\Cloud\Samples\Language\AnalyzeEntitiesCommand;
+use Google\Cloud\Samples\Language\AnalyzeSentimentCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new AnalyzeEntitiesCommand());
+$application->add(new AnalyzeSentimentCommand());
 $application->run();
