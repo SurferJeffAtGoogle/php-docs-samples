cedb5d284fccd59ef49369146dc1867314e3fc09
Jeffrey Rennie <rennie@google.com>
2016-09-16 14:37:21 -0700

    AnalyzeSyntax

--- language/api/language.php
+++ language/api/language.php
@@ -4,9 +4,11 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Google\Cloud\Samples\Language\AnalyzeEntitiesCommand;
 use Google\Cloud\Samples\Language\AnalyzeSentimentCommand;
+use Google\Cloud\Samples\Language\AnalyzeSyntaxCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new AnalyzeEntitiesCommand());
 $application->add(new AnalyzeSentimentCommand());
+$application->add(new AnalyzeSyntaxCommand());
 $application->run();
